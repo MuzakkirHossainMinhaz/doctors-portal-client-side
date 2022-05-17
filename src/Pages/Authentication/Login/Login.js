e493bf56d4f654193ae21dcba0f76a8a904de6dd
muzakkirhossainminhaz <minhaz.bauet@gmail.com>
2022-05-17 18:30:08 +0600

    login added

--- src/Pages/Authentication/Login/Login.js
+++ src/Pages/Authentication/Login/Login.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+    if (user) {
+        console.log(user);
+    }
+
     return (
-        <div>
-            <h1>From Login Page</h1>
+        <div className='flex justify-center items-center h-screen'>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="mb-2 font-semibold text-xl text-center">Login</h2>
+                    <div>
+                        <label className='text-start text-sm'>Email</label>
+                        <input type="email" placeholder="" class="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div>
+                        <label className='text-start text-sm'>Password</label>
+                        <input type="password" placeholder="" class="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <p className='text-[10px]'>Forgot Password ?</p>
+
+                    <button class="btn btn-accent text-base font-normal">Login</button>
+
+                    <p className='text-xs text-center mt-2'>New to Doctors Portal? <span className='text-secondary'>Create new account</span></p>
+
+                    <div class="divider">OR</div>
+
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        class="btn btn-outline btn-accent text-base font-normal">Continue with google</button>
+
+                </div>
+            </div>
         </div>
     );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,14 +5,30 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
+}; */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4k0rESK7FOavWsvi3nfFhiIFv0Ks90Hk",
+  authDomain: "doctors-portal-632f1.firebaseapp.com",
+  projectId: "doctors-portal-632f1",
+  storageBucket: "doctors-portal-632f1.appspot.com",
+  messagingSenderId: "1048398063436",
+  appId: "1:1048398063436:web:570b14244c0e09e87a51e9",
 };
+
+/* console.log(firebaseConfig.apiKey);
+console.log(firebaseConfig.authDomain);
+console.log(firebaseConfig.projectId);
+console.log(firebaseConfig.storageBucket);
+console.log(firebaseConfig.messagingSenderId);
+console.log(firebaseConfig.appId); */
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
